8a4da8b073a5b3c7db9ecafaade1fbff91863dd6
BoppreH <boppreh@gmail.com>
2014-12-12 18:25:06 -0200

    Change type of 'number'

--- ecc.c
+++ ecc.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 // Chunk of a finite field element.
 typedef unsigned char chunk;
 // Number of chunks per element.
 #define N 10
 // A finite field element.
-typedef struct { chunk v[N]; }* number;
+typedef chunk* number;
 // Helper macro.
 #define FOR(i) for (int i = 0; i < N; i++)
 
 number new_number() {
-    number n = malloc(sizeof(number));
+    number n = malloc(sizeof(chunk) * N);
     FOR(i) {
-        n->v[i] = 0x00;
+        n[i] = 0x00;
     }
     return n;
-}
-
-void print(char* label, number n) {
-    printf("%s: ", label);
-    for (int i = N - 1; i >= 0; i--) {
-        printf("%02x", n->v[i]);
-    } 
-    printf("\n");
 }
 
 void add(number a, number b, number result) {
     chunk carry = 0;
     chunk sum;
     FOR(i) {
-        sum = a->v[i] + b->v[i] + carry;
-        carry = sum < a->v[i] || (sum == a->v[i] && b->v[i] != 0);
-        result->v[i] = sum;
+        sum = a[i] + b[i] + carry;
+        carry = sum < a[i] || (sum == a[i] && b[i] != 0);
+        result[i] = sum;
     }
 }
 
@@ -40,27 +33,74 @@ void sub(number a, number b, number result) {
     chunk carry = 0;
     chunk sum;
     FOR(i) {
-        sum = a->v[i] - b->v[i] - carry;
-        carry = a->v[i] < b->v[i] || sum > a->v[i];
-        result->v[i] = sum;
+        sum = a[i] - b[i] - carry;
+        carry = a[i] < b[i] || sum > a[i];
+        result[i] = sum;
+    }
+}
+
+int eq(number a, number b) {
+    FOR(i) {
+        if (a[i] != b[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+//////////////////////
+// Testing function //
+//////////////////////
+
+void print(char* label, number n) {
+    printf("%s: ", label);
+    for (int i = N - 1; i >= 0; i--) {
+        printf("%02hhx", n[i]);
+    } 
+    printf("\n");
+}
+
+void assertEquals(number a, char* text) {
+    number b = new_number();
+    FOR(i) {
+        sscanf(text + i * 2, "%02hhx", &b[N-i-1]);
+    }
+    if (!eq(a, b)) {
+        print("Expected ", b);
+        print("got ", a);
     }
 }
 
 int main() {
     number a = new_number();
-    a->v[0] = 0x01;
+    a[0] = 0x01;
+    assert(eq(a, a));
+    assertEquals(a, "00000000000000000001");
+
     number b = new_number();
-    b->v[0] = 0xFF;
-    b->v[1] = 0xFF;
+    b[0] = 0xFF;
+    b[1] = 0xFF;
+    assertEquals(b, "0000000000000000FFFF");
+
     number c = new_number();
-    print("a", a);
-    print("b", b);
     add(a, b, c);
-    print("a+b", c);
+    assertEquals(c, "00000000000000010000");
+    add(a, c, c);
+    assertEquals(c, "00000000000000010001");
+    add(b, c, c);
+    assertEquals(c, "00000000000000020000");
     sub(c, b, c);
-    print("a+b-b", c);
-    c->v[0] = 0x00;
-    print("c", c);
+    assertEquals(c, "00000000000000010001");
+    sub(c, b, c);
+    assertEquals(c, "00000000000000000002");
     sub(c, a, c);
-    print("-a", c);
+    sub(c, a, c);
+    sub(c, a, c);
+    assertEquals(c, "FFFFFFFFFFFFFFFFFFFF");
+    add(c, a, c);
+    assertEquals(c, "00000000000000000000");
+
+    add(b, b, c);
+    assertEquals(c, "0000000000000001FFFE");
 }
