eaa6089b31288d8e73afed36f816d5a9c972a375
BoppreH <lucasboppre@gmail.com>
2015-03-27 18:32:45 -0300

    Add modulo operation to add and sub

--- ecc.c
+++ ecc.c
@@ -10,6 +10,11 @@ typedef unsigned char chunk;
 typedef chunk* number;
 // Helper macro.
 #define FOR(i) for (int i = 0; i < N; i++)
+#define ZERO new_number()
+
+number new_number();
+void add(number a, number b, number p, number result);
+void sub(number a, number b, number p, number result);
 
 number new_number() {
     number n = malloc(sizeof(chunk) * N);
@@ -19,7 +24,7 @@ number new_number() {
     return n;
 }
 
-void add(number a, number b, number result) {
+void add(number a, number b, number p, number result) {
     chunk carry = 0;
     chunk sum;
     FOR(i) {
@@ -27,9 +32,13 @@ void add(number a, number b, number result) {
         carry = sum < a[i] || (sum == a[i] && b[i] != 0);
         result[i] = sum;
     }
+
+    if (carry) {
+        sub(result, p, ZERO, result);
+    }
 }
 
-void sub(number a, number b, number result) {
+void sub(number a, number b, number p, number result) {
     chunk carry = 0;
     chunk sum;
     FOR(i) {
@@ -37,6 +46,14 @@ void sub(number a, number b, number result) {
         carry = a[i] < b[i] || sum > a[i];
         result[i] = sum;
     }
+
+    if (carry) {
+        add(result, p, ZERO, result);
+    }
+}
+
+void mult(number a, number b, number result) {
+    //number inter = (number) malloc(sizeof(chunk) * N * 2);
 }
 
 int eq(number a, number b) {
@@ -53,6 +70,14 @@ int eq(number a, number b) {
 // Testing function //
 //////////////////////
 
+number parse(char* text) {
+    number b = new_number();
+    FOR(i) {
+        sscanf(text + i * 2, "%02hhx", &b[N-i-1]);
+    }
+    return b;
+}
+
 void print(char* label, number n) {
     printf("%s: ", label);
     for (int i = N - 1; i >= 0; i--) {
@@ -62,10 +87,7 @@ void print(char* label, number n) {
 }
 
 void assertEquals(number a, char* text) {
-    number b = new_number();
-    FOR(i) {
-        sscanf(text + i * 2, "%02hhx", &b[N-i-1]);
-    }
+    number b = parse(text);
     if (!eq(a, b)) {
         print("Expected ", b);
         print("got ", a);
@@ -73,34 +95,32 @@ void assertEquals(number a, char* text) {
 }
 
 int main() {
-    number a = new_number();
-    a[0] = 0x01;
+    number a = parse("00000000000000000001");
     assert(eq(a, a));
     assertEquals(a, "00000000000000000001");
 
-    number b = new_number();
-    b[0] = 0xFF;
-    b[1] = 0xFF;
+    number b = parse("0000000000000000FFFF");
     assertEquals(b, "0000000000000000FFFF");
+    assert(!eq(a, b));
 
     number c = new_number();
-    add(a, b, c);
+    add(a, b, ZERO, c);
     assertEquals(c, "00000000000000010000");
-    add(a, c, c);
+    add(a, c, ZERO, c);
     assertEquals(c, "00000000000000010001");
-    add(b, c, c);
+    add(b, c, ZERO, c);
     assertEquals(c, "00000000000000020000");
-    sub(c, b, c);
+    sub(c, b, ZERO, c);
     assertEquals(c, "00000000000000010001");
-    sub(c, b, c);
+    sub(c, b, ZERO, c);
     assertEquals(c, "00000000000000000002");
-    sub(c, a, c);
-    sub(c, a, c);
-    sub(c, a, c);
+    sub(c, a, ZERO, c);
+    sub(c, a, ZERO, c);
+    sub(c, a, ZERO, c);
     assertEquals(c, "FFFFFFFFFFFFFFFFFFFF");
-    add(c, a, c);
+    add(c, a, ZERO, c);
     assertEquals(c, "00000000000000000000");
 
-    add(b, b, c);
+    add(b, b, ZERO, c);
     assertEquals(c, "0000000000000001FFFE");
 }
