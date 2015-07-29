9af3726f53d918496ab4fb73b2c344e28d5f4603
BoppreH <boppreh@gmail.com>
2015-07-29 22:19:14 +0000

    make_tests.py now compiles and runs the tests it generates

--- make_tests.py
+++ make_tests.py
@@ -3,54 +3,66 @@ from field import FieldValue
 from random import randint, choice
 import math
 
-prime = 2**31 -1
-n = math.ceil(math.log2(prime) / 8)
-F = lambda i: FieldValue(i, prime)
-h = lambda i: hex(i)[2:]
+def make_c_source(prime=2**31-1):
+    n = math.ceil(math.log2(prime) / 8) + 1
+    F = lambda i: FieldValue(i, prime)
+    h = lambda i: hex(i)[2:]
 
-def random():
-    """ Generates a number with a uniformly random number of digits. """
-    length = randint(1, math.ceil(math.log(prime, 16)))
-    str = ''.join(choice('0123456789ABCDEF') for i in range(length))
-    return F(int(str, 16))
+    def random():
+        """ Generates a number with a uniformly random number of digits. """
+        length = randint(1, math.ceil(math.log(prime, 16)))
+        str = ''.join(choice('0123456789ABCDEF') for i in range(length))
+        return F(int(str, 16))
 
-print("""
-#include "ecc.c"
+    yield """
+    #include "ecc.c"
 
-int main() {
-""")
-print('\tNumber p = parse("{}", {});'.format(h(prime), n))
-print('\tNumber result = new_number({});'.format(n))
-print()
+    int main() {
+    """
+    yield '\tNumber p = parse("{}", {});'.format(h(prime), n)
+    yield '\tNumber result = new_number({});'.format(n)
+    yield ''
 
-def make_test(a, b, expected, op):
-    print('\t{}(parse("{}", {}), parse("{}", {}), p, result);'.format(op, h(a), n, h(b), n))
-    print('\tprint(result);')
-    print('\tassertEquals(result, parse("{}", {}));'.format(h(expected), n))
-    print()
+    def make_test(a, b, expected, op):
+        yield '\t{}(parse("{}", {}), parse("{}", {}), p, result);'.format(op, h(a), n, h(b), n)
+        yield '\tassertEquals(result, parse("{}", {}));'.format(h(expected), n)
+        yield ''
 
-make_test(F(0), F(0), F(0), 'addm')
-make_test(F(0), F(1), F(1), 'addm')
-make_test(F(1), F(0), F(1), 'addm')
-make_test(F(1), F(1), F(2), 'addm')
+    yield from make_test(F(0), F(0), F(0), 'addm')
+    yield from make_test(F(0), F(1), F(1), 'addm')
+    yield from make_test(F(1), F(0), F(1), 'addm')
+    yield from make_test(F(1), F(1), F(2), 'addm')
 
-make_test(F(0), F(0), F(0), 'mulm')
-make_test(F(0), F(1), F(0), 'mulm')
-make_test(F(1), F(0), F(0), 'mulm')
-make_test(F(1), F(1), F(1), 'mulm')
+    yield from make_test(F(0), F(0), F(0), 'mulm')
+    yield from make_test(F(0), F(1), F(0), 'mulm')
+    yield from make_test(F(1), F(0), F(0), 'mulm')
+    yield from make_test(F(1), F(1), F(1), 'mulm')
 
-def make_op_test(n, function, name, b_can_be_zero=True):
-    for i in range(n):
-        a = random()
-        while True:
-            b = random()
-            if b or b_can_be_zero:
-                break
-        make_test(a, b, function(a, b), name)
+    def make_op_test(n, function, name, b_can_be_zero=True):
+        yield 'printf("Starting {} tests.\\n");'.format(name)
+        for i in range(n):
+            a = random()
+            while True:
+                b = random()
+                if b or b_can_be_zero:
+                    break
+            yield from make_test(a, b, function(a, b), name)
+        yield 'printf("Finished {} tests.\\n");'.format(name)
 
-make_op_test(1000, lambda a, b: a + b, 'addm')
-make_op_test(1000, lambda a, b: a - b, 'subm')
-make_op_test(1000, lambda a, b: a * b, 'mulm')
-make_op_test(1000, lambda a, b: a / b, 'divm', False)
+    yield from make_op_test(2000, lambda a, b: a + b, 'addm')
+    yield from make_op_test(2000, lambda a, b: a - b, 'subm')
+    yield from make_op_test(2000, lambda a, b: a * b, 'mulm')
+    yield from make_op_test(2000, lambda a, b: a / b, 'divm', False)
 
-print('}')
+    yield '}'
+
+import os
+
+for prime in [2**31-1, 2147483659, 4294967311, 4294967357, 3221225473]:
+    with open('automated_tests.c', 'w') as f:
+        for line in make_c_source(prime):
+            f.write(line + '\n')
+
+    print('Testing prime {}...'.format(prime))
+
+    os.system('clang -Wall automated_tests.c -o automated_tests && ./automated_tests && rm automated_tests automated_tests.c')
